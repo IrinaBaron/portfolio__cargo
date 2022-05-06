156bf42604404c3c4629c4acdb94cfd292330b6b
Irina Baron <irr.baron@gmail.com>
2022-05-06 16:00:04 +0300

    worked with sections header, hero

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   try {
+
+    const swiper1 = new Swiper('.hero__swiper', {
+
+      slidesPerView: 1,
+      spaceBetween: 30,
+      pagination: {
+        el: ' .swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.hero__swiper .swiper-button-next',
+        prevEl: '.hero__swiper .swiper-button-prev',
+      },
+      a11y: {
+        prevSlideMessage: 'Previous slide',
+        nextSlideMessage: 'Next slide',
+      },
+
+    });
     
   } catch (error) {
     console.log(error)
